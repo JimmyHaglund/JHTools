3b0e173240b2f39d49e2bd8b410f4b1b11fb9ab7
Jimmy <Jimmyhaglund@gmail.com>
2021-05-31 04:58:08 +0200

    Fix: Prevent out of range exeptions as a result of deactivating objects

--- Assets/Tools/Rapid/Events/ScriptableEvent.cs
+++ Assets/Tools/Rapid/Events/ScriptableEvent.cs
@@ -5,10 +5,16 @@ namespace JimmyHaglund.Rapid {
     public class ScriptableEvent<T> : ScriptableObject {
         private List<IEventListener<T>> _listeners = new List<IEventListener<T>>();
 
+        private bool _raising = false;
+        private List<IEventListener<T>> _queuedRemoves = new List<IEventListener<T>>();
+
         public void Raise(T data) {
+            _raising = true;
             for (int i = _listeners.Count - 1; i >= 0; i--) {
                 _listeners[i].OnEventRaised(data);
             }
+            _raising = false;
+            RemoveQueuedListeners();
         }
 
         public void RegisterListener(IEventListener<T> listener) {
@@ -17,7 +23,18 @@ namespace JimmyHaglund.Rapid {
         }
 
         public void UnRegisterListener(IEventListener<T> listener) {
+            if (_raising) {
+                _queuedRemoves.Add(listener);
+                return;
+            }
             _listeners.Remove(listener);
+        }
+
+        // In case a listener removes, e.g. deactivates, all listeners from list as a response.
+        private void RemoveQueuedListeners() {
+            foreach (IEventListener<T> removedListener in _queuedRemoves) {
+                _listeners.Remove(removedListener);
+            }
         }
     }
 }
